e07d457f2b209381353aa666ea676778255dfc7e
Uncle Bogdan <85982863+freshe4qa@users.noreply.github.com>
2023-01-13 20:33:24 +0200

    Update mars.sh

--- mars.sh
+++ mars.sh
@@ -91,7 +91,7 @@ sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0umars\"/" $HOME/.m
 
 # set peers and seeds
 SEEDS=""
-PEERS="6c855909a8bf1c12ef34baca059f5c0cdf82bc36@65.108.255.124:36656"
+PEERS="6c855909a8bf1c12ef34baca059f5c0cdf82bc36@65.108.255.124:36656,f3dceab155a74772595ae33ee6b72165c31fd888@62.171.166.106:26656"
 sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.mars/config/config.toml
 
 # config pruning
